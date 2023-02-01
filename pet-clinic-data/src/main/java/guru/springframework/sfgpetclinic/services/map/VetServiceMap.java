e939b1c94f1c4a3e67b1c5752c0fa45efcf371b1
alvaro <alvarorajic2@gmail.com>
2023-02-01 10:51:59 +0100

    added PetTypeService interface and its implementation PetTypeServiceMap. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -19,13 +19,13 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
     }
 
     @Override
-    public void delete(Vet entity) {
-        super.delete(entity);
+    public void delete(Vet object) {
+        super.delete(object);
     }
 
     @Override
-    public Vet save(Vet entity) {
-        return super.save(entity);
+    public Vet save(Vet object) {
+        return super.save(object);
     }
 
     @Override
